c9f210340c9b4e0ab83b047dc8c10dc77ccac711
Justin Edrique Reyes <55437786+reyesjustinedrique@users.noreply.github.com>
2022-04-13 02:43:48 +0800

    Updated about page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -14,10 +14,16 @@ const routes: Routes = [
   {
     path: 'schedules',
     loadChildren: () => import('./menu/schedules/schedules.module').then( m => m.SchedulesPageModule)
-  },  {
+  },
+  {
+    path: 'about-page',
+    loadChildren: () => import('./about-page/about-page.module').then( m => m.AboutPagePageModule)
+  },
+  {
     path: 'add-schedule',
     loadChildren: () => import('./menu/add-schedule/add-schedule.module').then( m => m.AddSchedulePageModule)
   }
+
 
 
 ];
