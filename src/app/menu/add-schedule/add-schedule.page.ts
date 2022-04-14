7889921ec8d67d692b372fd080e24e0929ec4910
DenversonCaacbay <denverkunfalcon@gmail.com>
2022-04-14 17:18:10 +0800

    Add Schedule

--- src/app/menu/add-schedule/add-schedule.page.ts
+++ src/app/menu/add-schedule/add-schedule.page.ts
@@ -1,4 +1,10 @@
+<<<<<<< Updated upstream
 import { Component, OnInit} from '@angular/core';
+=======
+import { Component, OnInit  } from '@angular/core';
+import { IonDatetime } from '@ionic/angular';
+import { format, parseISO } from 'date-fns';
+>>>>>>> Stashed changes
 @Component({
   selector: 'app-add-schedule',
   templateUrl: './add-schedule.page.html',
@@ -6,8 +12,13 @@ import { Component, OnInit} from '@angular/core';
 })
 export class AddSchedulePage implements OnInit {
   constructor() { }
+  dateValue2 = format(new Date(), 'hh:mm a') // sets default current time
 
   ngOnInit() {
   }
 
+  formatDate(time){
+    const formattedString = format(parseISO(time), 'hh:mm a');
+    return formattedString
+  }
 }
