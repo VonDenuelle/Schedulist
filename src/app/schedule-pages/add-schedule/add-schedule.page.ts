adb7185e3ca1d73cd2ed01cce6e769c545213a26
VonDenuelle <78338603+VonDenuelle@users.noreply.github.com>
2022-04-18 21:23:32 +0800

    UI Fixes #3

--- src/app/schedule-pages/add-schedule/add-schedule.page.ts
+++ src/app/schedule-pages/add-schedule/add-schedule.page.ts
@@ -12,6 +12,20 @@ export class AddSchedulePage implements OnInit {
   constructor(public menuCtrl: MenuController) { }
   
   dateValue2 = format(new Date(), 'hh:mm a') // sets default current time
+  taskStatus
+
+
+  days  = [
+    {day : 'Mon' , toggle : false},
+    {day : 'Tue' , toggle : false},
+    {day : 'Wed' , toggle : false},
+    {day : 'Thu' , toggle : false},
+    {day : 'Fri' , toggle : false},
+    {day : 'Sat' , toggle : false},
+    {day : 'Sun' , toggle : false},
+  ]
+ 
+  selectedDayId : number = 0; //default day toggled = none
 
   ngOnInit() {
   }
@@ -24,4 +38,11 @@ export class AddSchedulePage implements OnInit {
     return formattedString
   }
   
+  taskStatusChange(){
+    
+  }
+  toggleDay(day){
+    day.toggle == false ? day.toggle = true : day.toggle = false  
+  }
+ 
 }
