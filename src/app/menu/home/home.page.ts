b78eb64e542015e56b52f40f9a65d246d55f056e
pocholo818 <89579920+pocholo818@users.noreply.github.com>
2022-04-24 18:44:00 +0800

    modal list added
    -working but empty

--- src/app/menu/home/home.page.ts
+++ src/app/menu/home/home.page.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { ModalController } from '@ionic/angular';
+import { ListModalComponent } from '../list-modal/list-modal.component';
 
 @Component({
   selector: 'app-home',
@@ -6,10 +8,19 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.page.scss'],
 })
 export class HomePage implements OnInit {
+  
+  constructor(public modalController: ModalController) {}
 
-  constructor() { }
+  async presentModal() {
+    const modal = await this.modalController.create({
+      component: ListModalComponent,
+      cssClass: 'my-custom-class',
+    });
+    return await modal.present();
+  }
 
-  ngOnInit() {
+  ngOnInit(): void {
+    
   }
 
 }
