4a01c9fdc9f8b99756a8c21150575f119dbdf385
Justin Edrique Reyes <55437786+reyesjustinedrique@users.noreply.github.com>
2022-04-27 00:01:52 +0800

    Add files via upload

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,28 +1,15 @@
 import { Component } from '@angular/core';
-import { Router } from '@angular/router';
-import { AuthenticationService } from './services/authentication.service';
-import { UserService } from './services/users.services';
-
-
-
-
-
 @Component({
   selector: 'app-root',
   templateUrl: 'app.component.html',
   styleUrls: ['app.component.scss'],
 })
 export class AppComponent {
-
   public appPages = [
     { title: 'Home', url: '/home', icon: 'home' },
     { title: 'Schedules', url: '/schedules', icon: 'list' },
-    { title: 'About', url: '/about-page', icon: 'information-circle' }
+    { title: 'About', url: '/about-page', icon: 'information-circle' },
+    { title: 'Log out', url: '/logout', icon: 'log-out' }
   ];
-  constructor(private authService: AuthenticationService, private router: Router, public users: UserService) {}
- 
-  async logout() {
-    await this.authService.logout()
-    this.router.navigateByUrl('/', { replaceUrl: true })
-  }
+  constructor() {}
 }
